ed38b4fbae3e3e25de0189ae701f8cb4fe4f93fb
Alex Safonov <asafonov@drwsg.com>
2015-04-03 09:04:41 +0800

    seaworld sync script

---
+++ seaworld_status.sh
@@ -0,0 +1,24 @@
+
+srv=seaworld.synology.me
+
+dirs='music/albums/ videos/skydiving/ photos/publish/ photos/public/ photos/skydiving/'
+
+if [ "${1}k" != 'okk' ]
+then
+
+        dry_run="--dry-run"
+else
+        echo REAL RUN !
+
+fi
+
+
+cd $HOME
+
+for d in $dirs 
+do
+        echo processing $d
+        rsync -ah $d $srv:$d --delete $dry_run --info DEL,BACKUP,COPY,NAME
+        echo 
+
+done
