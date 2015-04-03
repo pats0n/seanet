11f54dee7b157ba08c1c2046f7c6781cd8533deb
Alex Safonov <asafonov@drwsg.com>
2015-04-03 10:48:24 +0800

    some ubuntu packages

--- ubuntu_pkgs.sh
+++ ubuntu_pkgs.sh
@@ -1,11 +1,25 @@
 #!/bin/sh
 
-sudo apt-get install vim git rsync ssh cifs-utils openconnect pssh nfs-common mercurial subversion golang s3cmd \
-        ec2-api-tools icedtea-netx \
+sudo apt-get install vim \
+        git \
+        rsync \
+        ssh 
+        cifs-utils \
+        openconnect \
+        pssh \
+        nfs-common \
+        mercurial \
+        subversion \
+        golang \
+        s3cmd \
+        ec2-api-tools 
+        icedtea-netx \
         nmap \
         mmv \
         iperf \
         atop \
         astyle  \
         pyflakes \
-        libxml2-utils
+        libxml2-utils\
+        screen
+
