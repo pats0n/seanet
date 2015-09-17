52ca315beed15e2310a5d2773e97ef5669cb6011
Alex Safonov <pats0nn@gmail.com>
2015-09-17 22:10:13 +0800

    added skipping message

--- seanet_pull.sh
+++ seanet_pull.sh
@@ -25,6 +25,8 @@ do
                 echo processing $d
                 rsync -ah $srv:$d $d --delete $dry_run --size-only --info DEL,BACKUP,COPY,NAME
                 echo 
+        else 
+                echo skipping $d
         fi
 
 
