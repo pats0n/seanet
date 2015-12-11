4bf5a959c80c1e5368681baedc06b2728b8faf38
Alex Safonov <pats0nn@gmail.com>
2015-12-11 13:47:27 +0800

    added date to git archive name

--- seanet_backup_git_to_gdrive.sh
+++ seanet_backup_git_to_gdrive.sh
@@ -2,8 +2,10 @@ cd $HOME
 
 mkdir -p ~/gitbackup
 
+date=$(date +%Y%m%d)
 
-tar -cf - git-repos/* | gzip - > ~/gitbackup/gitbackup.tar.gz
+
+tar -cf - git-repos/* | gzip - > ~/gitbackup/gitbackup.${date}.tar.gz
 
 $HOME/bin/drive push  -no-prompt=True gitbackup
 
