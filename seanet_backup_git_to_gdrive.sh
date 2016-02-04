7a7c4ac26ddb682046a49d81bb6c043e29424e50
Alex Safonov <pats0nn@gmail.com>
2016-02-04 20:14:21 +0800

    ...

--- seanet_backup_git_to_gdrive.sh
+++ seanet_backup_git_to_gdrive.sh
@@ -5,7 +5,7 @@ mkdir -p ~/gitbackup
 date=$(date +%Y%m%d)
 
 
-tar -cf - git-repos/* | gzip - > ~/gitbackup/gitbackup.${date}.tar.gz
+tar -cf - git-repos/* | gzip - > ~/gitbackup/gitbackup.$date.tar.gz
 
-$HOME/bin/drive push  -no-prompt=True gitbackup/gitbackup.${date}.tar.gz
+$HOME/bin/drive push  -no-prompt=True gitbackup/gitbackup.$date.tar.gz --quiet
 
