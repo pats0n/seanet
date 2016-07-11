60d269ca3a4a8d3f541d076d7ed3303100b9293b
Alex Safonov <pats0nn@gmail.com>
2016-07-11 21:05:21 +0800

    ...

--- seanet_backup_seasnail.sh
+++ seanet_backup_seasnail.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-rsync -va --delete $HOME/vm/seasnail/ seanet:vm/seasnail$(date +%Y%m%d)/ --progress
+rsync -va --delete $HOME/vm/seasnail/ seanet:vm/seasnail_$(date +%Y%m%d)/ --progress
