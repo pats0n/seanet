7a19b135e0a9df44cc09599ebffbbc66ffb8842c
Alex Safonov <pats0nn@gmail.com>
2016-09-09 18:16:21 +0800

    added projects/tfndata/ctpticks

--- seanet_push_to_gdrive.sh
+++ seanet_push_to_gdrive.sh
@@ -1,4 +1,4 @@
-dirs='videos/skydiving photos/skydiving music/albums photos/publish photos/public'
+dirs='videos/skydiving photos/skydiving music/albums photos/publish photos/public projects/tfndata/ctpticks'
 
 for d in $dirs 
 do
