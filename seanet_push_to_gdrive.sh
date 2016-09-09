756eaec009cf865ede1bc4504cd4164b8db12a19
Alex Safonov <pats0nn@gmail.com>
2016-09-09 15:19:52 +0800

    ...

--- seanet_push_to_gdrive.sh
+++ seanet_push_to_gdrive.sh
@@ -1,4 +1,4 @@
-dirs='videos/skydiving photos/skydiving music/albums photos/publish photos/public bvrt'
+dirs='videos/skydiving photos/skydiving music/albums photos/publish photos/public'
 
 for d in $dirs 
 do
