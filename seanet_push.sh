1fe77ae96f513fbd127f438bd4e0fb4e0a83deea
Alex Safonov <alexander.safonov@ingensoma.com>
2017-11-02 15:17:29 +0800

    ...

--- seanet_push.sh
+++ seanet_push.sh
@@ -1,7 +1,7 @@
 
 srv=seanet
 
-dirs='music/albums/ videos/skydiving/ videos/flying/ photos/publish/ photos/public/ photos/skydiving/ flysight/'
+dirs='music/albums/ videos/skydiving/ videos/flying/ photos/publish/ photos/public/ photos/skydiving/ flysight/ openshot/'
 
 if [ "${1}k" != 'okk' ]
 then
