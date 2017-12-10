8675075251d8277be2d7de78a0f6ba5c6f6f3bb6
Alex Safonov <pats0nn@gmail.com>
2017-12-10 10:15:19 +0800

    ...

--- seanet_push.sh
+++ seanet_push.sh
@@ -1,7 +1,7 @@
 
 srv=seanet
 
-dirs='music/albums/ videos/skydiving/ videos/flying/ photos/publish/ photos/public/ photos/skydiving/ flysight/ videos/openshot/ photos/lrcat/'
+dirs='music/albums/ videos/skydiving/ videos/flying/ photos/publish/ photos/public/ photos/skydiving/ flysight/ videos/openshot/ photos/lrcat/ videos/dr/'
 
 if [ "${1}k" != 'okk' ]
 then
