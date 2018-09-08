b624f24cf504be91efb4ea9285216856737288b7
Alex <pats0nn@gmail.com>
2018-09-08 14:31:05 +0800

    ...

--- seanet_pull.sh
+++ seanet_pull.sh
@@ -1,7 +1,14 @@
 
 srv=seanet
 
-dirs='music/albums/ videos/skydiving/ videos/flying/ photos/publish/ photos/public/ photos/skydiving/ flysight/ videos/openshot/ videos/dr/'
+dirs='music/albums/'
+dirs+=' videos/skydiving/'
+dirs+=' videos/flying/'
+#dirs+=' photos/publish/'
+#dirs+=' photos/public/'
+dirs+=' photos/skydiving/'
+dirs+=' flysight/'
+dirs+=' videos/dr/'
 
 if [ "${1}k" != 'okk' ]
 then
