a1baf516d0db1507fc272bbf32461a6d74a2ab98
Alex <pats0nn@gmail.com>
2020-12-21 03:38:29 +0800

    ...

--- seanet_push_to_gdrive_2.sh
+++ seanet_push_to_gdrive_2.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
 dirs=
-dirs+=" projects/huodata" 
 dirs+=" projects/m3_ticks"
+dirs+=" projects/m3_capture"
+dirs+=" projects/huodata" 
 dirs+=" projects/bfldata" 
 dirs+=" projects/bfxdata" 
 dirs+=" projects/bmxdata" 
@@ -11,16 +12,15 @@ dirs+=" projects/lqddata"
 dirs+=" projects/okxdata" 
 dirs+=" projects/btsedata" 
 
-dirs+=" projects/bfl_states" 
-dirs+=" projects/srv02_states" 
-dirs+=" projects/srv03_states" 
+# dirs+=" projects/bfl_states" 
+# dirs+=" projects/srv02_states" 
+# dirs+=" projects/srv03_states" 
 
 # dirs+=" projects/btc_zabbix_backup"
 # dirs+=" projects/btc_jenkins_backup"
 # dirs+=" projects/btc_influx_backup"
 # dirs+=" projects/btc_grafana_backup"
 
-dirs+=" projects/m3_capture"
 
 for d in $dirs 
 do
