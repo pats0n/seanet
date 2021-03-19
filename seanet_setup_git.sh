835d73c37dbccda452a3b3bc01914c398cadfc9a
Alex <pats0nn@gmail.com>
2021-03-19 08:35:48 +0000

    ...

--- seanet_setup_git.sh
+++ seanet_setup_git.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-
 git config --global push.default simple
 git config --global user.email pats0nn@gmail.com
 git config --global user.name Alex Safonov
+git config --global credential.helper store
