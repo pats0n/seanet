5f10ca2c734f2184b798f5e1aa9b17e841a12b53
Alex <pats0nn@gmail.com>
2021-06-01 05:12:31 +0000

    ...

--- seanet_setup_git.sh
+++ seanet_setup_git.sh
@@ -3,4 +3,4 @@
 git config --global push.default simple
 git config --global user.email pats0nn@gmail.com
 git config --global user.name Alex Safonov
-git config --global credential.helper store
+git config --global credential.helper "cache  --timeout 3600"
