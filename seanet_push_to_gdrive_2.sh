421f66aa3a63782afbaff1c43635fe4663741d44
Alex <pats0nn@gmail.com>
2021-12-06 08:36:06 +0000

    ...

--- seanet_push_to_gdrive_2.sh
+++ seanet_push_to_gdrive_2.sh
@@ -1,31 +1,24 @@
 #!/bin/bash
 
-
 cd $HOME
 
 dirs=
-dirs+=`find -L  projects/m3_ticks -mindepth 1 -maxdepth 1 -type d`
-dirs+=`find -L  projects/m3_capture -mindepth 1 -maxdepth 1 -type d`
-dirs+=" projects/huodata" 
-dirs+=" projects/bfldata" 
-dirs+=" projects/bfxdata" 
-dirs+=" projects/bmxdata" 
-dirs+=" projects/btsedata" 
-# dirs+=" projects/bnbdata" 
-# dirs+=" projects/lqddata" 
-# dirs+=" projects/okxdata" 
 
-# dirs+=" projects/bfl_states" 
-# dirs+=" projects/srv02_states" 
-# dirs+=" projects/srv03_states" 
+# dirs+=" projects/huodata" 
+# dirs+=" projects/bfldata" 
+# dirs+=" projects/bmxdata" 
+# dirs+=" projects/btsedata" 
 
-dirs+=" projects/btc_influx_backup"
-dirs+=" projects/btc_jenkins_backup"
+# dirs+=`find -L  projects/m3_ticks -mindepth 1 -maxdepth 1 -type d`
+# dirs+=`find -L  projects/m3_capture -mindepth 1 -maxdepth 1 -type d`
 
-dirs+=" projects/backup_grafana_analytics01"
-dirs+=" projects/backup_influx_analytics01"
-dirs+=" projects/backup_jenkins01"
-dirs+=" projects/backup_zabbix01"
+# dirs+=" projects/btc_influx_backup"
+# dirs+=" projects/btc_jenkins_backup"
+
+# dirs+=" projects/backup_grafana_analytics01"
+# dirs+=" projects/backup_influx_analytics01"
+# dirs+=" projects/backup_jenkins01"
+# dirs+=" projects/backup_zabbix01"
 
 
 for d in $dirs 
